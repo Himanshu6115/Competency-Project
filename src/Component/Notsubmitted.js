3064865df511cc2cb0c9891e85d5022abffe7dc6
Himanshu6115 <86473642+Himanshu6115@users.noreply.github.com>
2021-07-26 10:27:16 +0530

    Add files via upload

---
+++ src/Component/Notsubmitted.js
@@ -0,0 +1,108 @@
+import React, {useState, useEffect} from 'react'
+import ReactDOM from 'react-dom'
+import axios from 'axios'
+import Navbartwo from './Navbartwo.js';
+import '../CSS/NotApproved.css'; 
+
+ 
+
+ 
+ function Notsubmitted() {
+    var id ="";
+    var a;
+    
+    const [supervisor,setsupervisor]=useState(null);
+    const [data,setdata]=useState(null);
+    
+
+ 
+
+  const fetchData = async () => {
+    const response = await axios.get(
+      'http://localhost:8080/notSubmitted?supervisorName='+supervisor
+    );
+
+ if(supervisor==null)
+{alert("please enter the supervisor name");}
+
+    setdata(response.data);
+    
+  };
+   function dp(){
+    if(id!="")
+    {alert("E-mail Ids are copied")}
+  else{
+    alert("No Email to copy")
+  }
+    
+
+} 
+
+ 
+
+  return (
+    <div className="App">
+       <section className="header">
+      <section className="header-top">
+        <section className="header-top__navbar">
+          <section className="header-top__navigation">
+            <Navbartwo />
+          </section>          
+        </section>
+        </section>          
+        </section>
+      <label id="id">Global ID </label>
+       <input type="text" placeholder="Global ID" id ="globalID" value={supervisor}  onChange={(e)=> setsupervisor(e.target.value)}></input>
+      <div>
+        <button className="fetch-button" onClick={fetchData}>
+          Fetch Data
+        </button>
+         <br />
+        <br />
+        <button id="btn11"
+  onClick={() =>  {navigator.clipboard.writeText(id); dp();} } >
+  Copy E-mail
+</button>
+        <br />
+      </div>
+        <table id="users">
+    <thead>
+    <tr>
+    <th>GLOBALID</th>
+    <th>EMPLOYEE NAME</th>
+    <th>EMAILID</th>
+ 
+ </tr>
+ </thead>
+ <tbody>
+      
+        {data &&
+          data.map(d => {
+            if(d.email!="")
+            {
+                id=id+d.email+";"
+                console.log(id)
+            }
+
+ 
+
+            return (
+         <tr>
+        <td>{d.empId}</td>
+        <td>{d.empName}</td>
+        <td>{d.email}</td>
+
+        </tr>
+            );
+          })}
+</tbody>
+ </table>
+      <br/>
+      <br/>
+      <br/>
+      <br/>
+       
+    </div>
+  );
+}
+export default Notsubmitted;
