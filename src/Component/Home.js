1b1b6eb4db9b4c35b69d2fdf1014da586fab3885
Himanshu6115 <86473642+Himanshu6115@users.noreply.github.com>
2021-08-01 22:24:36 +0530

    Add files via upload

---
+++ src/Component/Home.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import Header from '../Component/Header.js';
+import Footer from '../Component/Footer.js';
+import '../CSS/Home.css';
+
+function Home () {
+    return (
+    <div className="home">
+    <Header />
+    <Footer />    
+    </div>
+    
+  );
+
+
+}
+
+export default Home;
